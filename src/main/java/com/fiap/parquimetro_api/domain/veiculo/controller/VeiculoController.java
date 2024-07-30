6177eb5e2d0b5168aa414d129d9633bbac8a7865
rmstrunkis <rmstrunkis@gmail.com>
2024-07-29 23:39:49 -0300

    Criando metodo de busca e atualizacao do veiculo

--- src/main/java/com/fiap/parquimetro_api/domain/veiculo/controller/VeiculoController.java
+++ src/main/java/com/fiap/parquimetro_api/domain/veiculo/controller/VeiculoController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/veiculo")
 public class VeiculoController {
@@ -14,7 +16,22 @@ public class VeiculoController {
     private VeiculoService veiculoService;
 
     @PostMapping("/cadastrar")
-    public ResponseEntity<VeiculoDTO> cadastrarVeiculo(@RequestBody VeiculoDTO veiculoDTO, @RequestParam String cpf){
+    public ResponseEntity<VeiculoDTO> cadastrar (@RequestBody VeiculoDTO veiculoDTO, @RequestParam String cpf){
         return veiculoService.cadastrar(cpf, veiculoDTO);
+    }
+
+    @GetMapping ("/listar/{placa}")
+    public ResponseEntity<VeiculoDTO> consultar (@PathVariable String placa){
+        return veiculoService.consultar(placa);
+    }
+
+    @GetMapping ("/listar")
+    public ResponseEntity<List<VeiculoDTO>> listar (){
+        return veiculoService.listar();
+    }
+
+    @PutMapping("/atualizar")
+    public ResponseEntity<VeiculoDTO> atualizar (@RequestBody VeiculoDTO veiculoDTO, @RequestParam String cpf){
+        return veiculoService.atualizar(veiculoDTO, cpf);
     }
 }
